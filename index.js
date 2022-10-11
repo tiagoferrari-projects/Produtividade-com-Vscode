16cc9ad81cbe491868bf7d02668cbe7ed5cdee02
Tiago Ferrari <tiago-projects@outlook.com>
2022-10-11 00:43:44 -0300

    commit final

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+//TODO teste1 
+
+
 function helloWorld(){
     console.log("Hello World!");
 }
+
+// FIXME teste 2
 
 const saudacao = () => {
     var data = new Date()
     return  data.getHours() <= 12? "Bom dia": data.getHours() <= 18? "Boa tarde": "Boa noite";
 }
-
+ 
+// TAG nova tag
 // demonstração comentário
 helloWorld();
 console.log('A saudação do momento é ' + saudacao());
